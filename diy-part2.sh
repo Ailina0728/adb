8877f600992dcbed8c3ba7aba530cd59dd44fe0a
Ailina0728 <baiyue7@gmail.com>
2021-03-25 21:23:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/liuran001/openwrt-packages
+git clone https://github.com/liuran001/openwrt-packages package
+git clone https://github.com/281677160/openwrt-package package
 
