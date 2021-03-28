6b8a08489b8ee4316e5f7c55b046dedcfef3667a
Ailina0728 <baiyue7@gmail.com>
2021-03-28 15:17:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,11 +11,17 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
-sed -i '$a src-git darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
+sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
 sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+sed -i '$a svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook' feeds.conf.default
+sed -i '$a svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocks-rust' feeds.conf.default
+sed -i '$a src-githttps://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
+sed -i '$a src-githttps://github.com/jerrykuku/lua-maxminddb.git' feeds.conf.default
+sed -i '$a src-githttps://github.com/jerrykuku/luci-app-vssr.git' feeds.conf.default
+sed -i '$a src-githttps://github.com/vernesong/OpenClash.git' feeds.conf.default
