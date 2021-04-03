a6c62cfee268c558a4b783f84675b4c401e7cfd8
Ailina0728 <baiyue7@gmail.com>
2021-04-03 22:48:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,9 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+
 sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
 sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
-sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
-sed -i '$a src-git jerrykuku https://github.com/jerrykuku/lua-maxminddb.git' feeds.conf.default
-sed -i '$a src-git Ailina0728 https://github.com/Ailina0728/luci-app-vssr' feeds.conf.default
+
+
+
