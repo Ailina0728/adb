19d9aa61b5ac45907b9b70d42d368a05bb4997d4
Ailina0728 <baiyue7@gmail.com>
2021-04-13 22:30:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
+
 sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
 
 # Add luci-app-passwall
@@ -96,7 +97,7 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter
 
 # Use Lienol's https-dns-proxy package
 pushd feeds/packages/net
-rm -rf https-dns-proxy
+rm -rf https_dns_proxy
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
 popd
 
@@ -118,4 +119,36 @@ git clone --depth=1 https://github.com/AdguardTeam/AdGuardHome
 #export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
 #sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
 #popd
+
+# Add luci-app-adguardhome
+cp -r temp/luci/applications/luci-app-adguardhome luci/applications/luci-app-adguardhome
+cp -r temp/packages/net/adguardhome packages/net/adguardhome
+cp -r temp/packages/lang/node-yarn packages/lang/node-yarn
+cp -r temp/packages/devel/packr packages/devel/packr
+
+# Add luci-app-gowebdav
+#cp -r temp/luci/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+#cp -r temp/packages/net/gowebdav packages/net/gowebdav
+
+# Add netdata
+rm -rf packages/admin/netdata
+rm -rf ../package/lean/luci-app-netdata
+cp -r temp/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
+cp -r temp/packages/admin/netdata packages/admin/netdata
+
+# Add luci-app-smartdns
+#cp -r temp/luci/applications/luci-app-smartdns luci/applications/luci-app-smartdns
+#cp -r temp/packages/net/smartdns packages/net/smartdns
+
+# Add tmate
+#cp -r temp/packages/net/tmate packages/net/tmate
+#cp -r temp/packages/libs/msgpack-c packages/libs/msgpack-c
+
+# Add gotop
+#cp -r temp/packages/admin/gotop packages/admin/gotop
+
+# Add minieap
+cp -r temp/packages/net/minieap packages/net/minieap
+rm -rf temp
+popd
 
