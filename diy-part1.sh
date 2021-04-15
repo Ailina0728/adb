a51a620bac489d8b32f32b47eb4287fdc3ba93e0
Ailina0728 <baiyue7@gmail.com>
2021-04-15 15:24:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,18 +11,14 @@
 #
 
 # Add luci-theme-argon
-pushd package/lean
-rm -rf luci-theme-argon
+rm -rf package/lean/luci-theme-argon
 sed -i '$a src-git -b 18.06 https://github.com/jerrykuku/luci-theme-argon' feeds.conf.default
 sed -i '$a src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config' feeds.conf.default
-popd
 
 # Add luci-app-dockerman
-pushd package/lean
-rm -rf luci-app-docker
+rm -rf package/leanluci-app-docker
 sed -i '$a src-git dockerman https://github.com/KFERMercer/luci-app-dockerman' feeds.conf.default
 sed -i '$a src-git docker https://github.com/lisaac/luci-lib-docker' feeds.conf.default
-popd
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
