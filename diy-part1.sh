662458edd98af041333f98c445b3846a90ab5874
Ailina0728 <baiyue7@gmail.com>
2021-04-15 16:33:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,8 +7,8 @@
 #sed -i '$a src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config' feeds.conf.default
 
 # Add luci-app-dockerman
-sed -i '$a src-git dockerman https://github.com/KFERMercer/luci-app-dockerman' feeds.conf.default
-sed -i '$a src-git docker https://github.com/lisaac/luci-lib-docker' feeds.conf.default
+git clone https://github.com/KFERMercer/luci-app-dockerman
+sed -i '$a src-git lisaac https://github.com/lisaac/luci-lib-docker' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
@@ -19,24 +19,23 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter' feeds.conf.default
 
 # Add mentohust & luci-app-mentohust
-sed -i '$a src-git mentohust https://github.com/BoringCat/luci-app-mentohust' feeds.conf.default
+sed -i '$a src-git BoringCat https://github.com/BoringCat/luci-app-mentohust' feeds.conf.default
 sed -i '$a src-git KyleRicardo https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk' feeds.conf.default
 
 # Add luci-app-onliner
-sed -i '$a src-git onliner https://github.com/rufengsuixing/luci-app-onliner' feeds.conf.default
+sed -i '$a src-git rufengsuixing https://github.com/rufengsuixing/luci-app-onliner' feeds.conf.default
 
 # Add luci-theme-rosy
 sed -i '$a src-git rosy https://github.com/rosywrt/luci-theme-rosy' feeds.conf.default
 
 # Add luci-app-ttnode
-sed -i '$a src-git ttnode https://github.com/jerrykuku/luci-app-ttnode' feeds.conf.default
+sed -i '$a src-git jerrykuku https://github.com/jerrykuku/luci-app-ttnode' feeds.conf.default
 
 # Add subconverter
-sed -i '$a src-git subconverter https://github.com/tindy2013/openwrt-subconverter' feeds.conf.default
+sed -i '$a src-git tindy2013 https://github.com/tindy2013/openwrt-subconverter' feeds.conf.default
 
 # Add luci-udptools
-sed -i '$a src-git udp https://github.com/zcy85611/openwrt-luci-kcp-udp' feeds.conf.default
+sed -i '$a src-git zcy85611 https://github.com/zcy85611/openwrt-luci-kcp-udp' feeds.conf.default
 
 # Add OpenAppFilter
-sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' feeds.conf.default
-
+git clone https://github.com/destan19/OpenAppFilter.git
