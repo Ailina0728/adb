bf3c4b3ae0077a773139c2914fdc6945c24ed7cb
Ailina0728 <baiyue7@gmail.com>
2021-04-16 18:56:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,15 +29,13 @@ popd
 #sed -i '$a src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config' feeds.conf.default
 
 # Add luci-app-dockerman
-#sed -i '$a src-git https://github.com/KFERMercer/luci-app-dockerman' feeds.conf.default
-#sed -i '$a src-git lisaac https://github.com/lisaac/luci-lib-docker' feeds.conf.default
-
+sed -i '$a src-git https://github.com/KFERMercer/luci-app-dockerman' feeds.conf.default
+sed -i '$a src-git lisaac https://github.com/lisaac/luci-lib-docker' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 pushd openwrt/feeds/small8
 rm -rf adguardhome
