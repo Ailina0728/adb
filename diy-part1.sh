c0d034f0ef8d2033abc9cdbc91df67784f01c661
Ailina0728 <baiyue7@gmail.com>
2021-04-16 16:10:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -38,14 +38,14 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-git clone https://github.com/kenzok8/small-package package/small-package
-pushd package/feeds/small-package
-rm -rf package/feeds/small-package/adguardhome
-rm -rf package/feeds/small-package/luci-app-adguardhome
-rm -rf package/feeds/small-package/luci-app-aliddns
-rm -rf package/feeds/small-package/luci-app-gost
-rm -rf package/feeds/small-package/luci-app-bypass
-popd
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+#pushd package/feeds/small-package
+#rm -rf package/feeds/small-package/adguardhome
+#rm -rf package/feeds/small-package/luci-app-adguardhome
+#rm -rf package/feeds/small-package/luci-app-aliddns
+#rm -rf package/feeds/small-package/luci-app-gost
+#rm -rf package/feeds/small-package/luci-app-bypass
+#popd
 # Add luci-theme-darkmatter
 sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter' feeds.conf.default
 
